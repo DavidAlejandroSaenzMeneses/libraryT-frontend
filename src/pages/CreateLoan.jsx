453cd4c66b3d620936f090fbcf3670abf236449d
DavidAlejandroSaenzMeneses <dasm17.as@gmail.com>
2022-02-20 22:12:20 -0500

    Ajuste UI
    -Se realiza ajustes en el modulo de prestamos

--- src/pages/CreateLoan.jsx
+++ src/pages/CreateLoan.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState, useRef } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import defaultPicture from '../assets/image/default.png';
 
-export default function Balance() {
-    const numberUser = useRef(); 
-    const typeUser = useRef();
+export default function CreateLoan() {
+    const navigate = useNavigate();
+    const MySwal = withReactContent(Swal);
     const { idBook } = useParams();
+    const numberUserRef = useRef();
+    const typeUserRef = useRef();
+    const nameUserRef = useRef();
+    const phoneUserRef = useRef();
+
+
+
     const documents = ['CC', 'CE', 'TI'];
     const url = 'http://localhost:3900/api/v1';
+    const urlBase = "http://localhost:3900/api/v1";
     //const [books, setBook] = useHttp(url);
     const [book, setBook] = useState({ book: false, status: '' });
-    const [user, setUser] = useState();
+    const [userData, setUser] = useState({ status: 'pending', user: false, message: '' });
     useEffect(() => {
         const getData = () => {
-            axios.get('http://localhost:3900/api/v1/books/' + idBook).then(res => {
+            axios.get(`${urlBase}/books/${idBook}`).then(res => {
                 setBook(res.data.result);
             }).catch(error => {
                 setBook({
@@ -27,124 +37,128 @@ export default function Balance() {
     }, []);
     const getUser = async (e) => {
         e.preventDefault();
-        const identificationUser=typeUser.current.value+'-'+numberUser.current.value;
-        //await axios.get("/api/updatecart", { data: { product: this.product } })
-        axios.get('http://localhost:3900/api/v1/users/'+identificationUser).then(res => {
-            setUser(res.data.result);
-        }).catch(error => {
-            setUser({
-                user: false,
-                status: 'success'
+        if (numberUserRef.current.value !== '') {
+            const identificationUser = `${typeUserRef.current.value}-${numberUserRef.current.value}`;
+            axios.get(`${urlBase}/users?identification=${identificationUser}`).then(res => {
+                setUser(res.data.result);
+                setUser({
+                    status: 'success',
+                    user: res.data.result,
+                    message: 'Usuario Registrado Previamente'
+                });
+                nameUserRef.current.value = res.data.result.full_name;
+                phoneUserRef.current.value = res.data.result.phone_number;
+            }).catch(error => {
+                setUser({
+                    status: 'success',
+                    user: false,
+                    message: 'Usuario Nuevo'
+                });
+                nameUserRef.current.value = '';
+                phoneUserRef.current.value = '';
             });
-        });
+        }
     };
     const saveLoan = async (e) => {
-        /*e.preventDefault();
-        changeState();
-        const newArticle = await axios.post(urlApi + '/save', state.article);
-        if (newArticle.data.article) {
-            setNewArticle({
-                article: newArticle.data.article,
-                status: 'waiting'
-            })
-            if (selectedFile !== null) {
-                //obtener el id del articulo guardado
-                const articleId = newArticle.data.article._id;
-                //crear un form data y añadirlo al fichero
-                const formData = new FormData();
-                formData.append(
-                    'file0',
-                    selectedFile,
-                    selectedFile.name
-                );
-                //peticion
-                axios.post(urlApi + 'upload-image/' + articleId, formData).then((res) => {
-                    if (res.data.articleUpdated) {
-                        setNewArticle({
-                            article: res.data.article,
-                            status: 'success'
-                        })
-                        navigate("/blog", { replace: true });
-                    } else {
-                        setNewArticle({
-                            article: {},
-                            status: 'failed'
-                        })
-                    }
-                })
-            }else{
-                navigate("/blog", { replace: true });
+        e.preventDefault();
+        if (numberUserRef.current.value === '' || nameUserRef.current.value === '' || phoneUserRef.current.value === '') {
+            MySwal.fire('Error', 'Datos incompletos', 'error');
+            return false;
+        }
+        const formDataUser = {
+            id_library_user: (userData.user.id_library_user?userData.user.id_library_user:null),
+            identification: `${typeUserRef.current.value}-${numberUserRef.current.value}`,
+            full_name: nameUserRef.current.value,
+            phone_number: phoneUserRef.current.value
+        };
+        try {
+            let updatedUserData = false;
+            let libraryUser= false;
+            if (userData.user.id_library_user !== false && userData.user.id_library_user!==undefined) {
+               updatedUserData = await axios.put(`${urlBase}/users/${userData.user.id_library_user}`, formDataUser);
+               libraryUser=userData.user.id_library_user;
+            } else {
+               updatedUserData = await axios.post(`${urlBase}/users`, formDataUser);
+               libraryUser=updatedUserData.data.result.id_library_user;
             }
-            MySwal.fire('Articulo Creado','El articulo fue creado correctamente','success');
-        } else {
-            setNewArticle({
-                article: {},
-                status: 'failed'
-            });
-        }*/
+            if(updatedUserData!==false){
+                const loansData={id_library_user:libraryUser,id_book:idBook};
+                updatedUserData = await axios.post(`${urlBase}/loans`, loansData);
+            }
+            MySwal.fire('Proceso Realizado', 'Prestamo realizado correctamente', 'success');
+            navigate("/books", { replace: true });
+        } catch (error) {
+            MySwal.fire('Error', 'Algo salio mal', 'error');
+        }
     }
     return (
         <div className="py-4 px-6 h-full">
-            <div className="title-Page flex items-center mb-4">
-                <h1 className="flex text-gray-700 text-xl font-bold">Nuevo Prestamo</h1>
-            </div>
-            <div className="data-records block p-2 bg-white w-full rounded-md shadow-lg h-xl justify-center">
-                {(book && book.title) ? (
+            <div className="data-records block p-2 bg-white w-full rounded-md shadow-lg h-4/5 justify-center">
+                {
+                    (book && book.title) ? (
 
-                    <div className="book-list-item flex h-sm p-2 my-2 h-60 bg-gray-100">
-                        <div className="book-image w-1/5">
-                            <div className="flex h-60 items-center">
-                                <div className="block">
-                                    {book.image !== null ? (
-                                        <img src={url + '/books/get-image/' + book.image} alt={book.title} className="object-scale-down h-48 w-96" />
-                                    ) : (
-                                        <img src={defaultPicture} alt="book" className="object-scale-down h-48 w-96" />
-                                    )}
-                                    <h3 className="text-xl font-bold text-gray-700 text-left">{book.title}</h3>
+                        <div className="book h-sm p-2 my-2 h-full items-center">
+                            <h3 className="text-xl font-bold mb-5 text-gray-700">Formulario de Prestamo</h3>
+                            <div className="flex justify-center">
+                                <div className="book-image w-1/5">
+                                    <div className="flex h-60 items-center">
+                                        <div className="block border border-gray-300 rounded-md">
+                                            {book.image !== null ? (
+                                                <img src={`${urlBase}/books/get-image/${book.image}`} alt={book.title} className="object-scale-down h-48 w-96" />
+                                            ) : (
+                                                <img src={defaultPicture} alt="book" className="object-scale-down h-48 w-96" />
+                                            )}
+                                            <h3 className="text-xl font-bold text-gray-700 text-center bg-gray-200">{book.title}</h3>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="book-info flex justify-center justify-items-center items-center w-4/5">
-                            <div className="block w-full ">
-                                <div className=" h-40 px-2 justify-center text-center">
-                                    <h3 className="text-xl font-bold mb-5">Formulario de Prestamo</h3>
-                                    <form onSubmit={saveLoan}>
-                                        <div className="form-group grid grid-cols-2 my-2">
-                                            <label htmlFor="title" className="text-gray-700 font-bold">Identificacion</label>
-                                            <div className="flex gap-2">
-                                                <select className="w-1/5 rounded-md" ref={typeUser}>
-                                                    {
-                                                        documents.map((document) => {
-                                                            return <option value={document}>{document}</option>
-                                                        })
-                                                    }
-                                                </select>
-                                                <input className="w-4/5 rounded-md" type="text" name="title" ref={numberUser} onBlur={getUser}/>
+                                <div className="book-info flex justify-center justify-items-center items-center w-3/5">
+                                    <div className="flex h-full px-2 justify-center text-center items-center">
+                                        <form onSubmit={saveLoan}>
+                                            <div className="form-group grid grid-cols-2 my-2">
+                                                <label htmlFor="title" className="text-left text-gray-700 font-bold">Identificacion</label>
+                                                <div className="block">
+                                                    <div className="w-full flex gap-2">
+                                                        <select className="border border-gray-500 w-1/5 rounded-md" ref={typeUserRef}>
+                                                            {
+                                                                documents.map((document, i) => {
+                                                                    return <option key={i} value={document}>{document}</option>
+                                                                })
+                                                            }
+                                                        </select>
+                                                        <input className="border border-gray-500 p-1 w-4/5 rounded-md" type="text" name="title" ref={numberUserRef} onBlur={getUser} />
+                                                    </div>
+                                                    <div className="w-full text-center mb-2">
+                                                        {(userData && userData.message !== '' && userData.user !== false) &&
+                                                            <p className="text-md text-green-700">{userData.message}</p>
+                                                        }
+                                                        {(userData && userData.message !== '' && userData.user === false) &&
+                                                            <p className="text-md text-yellow-700">{userData.message}</p>
+                                                        }
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="form-group grid grid-cols-2 my-2">
-                                            <label htmlFor="title" className="text-gray-700 font-bold">Nombre Completo</label>
-                                            <input className="w-full rounded-md" type="text" name="title"  value={user.full_name}/>
-                                        </div>
-                                        <div className="form-grou grid grid-cols-2 my-2 mb-4">
-                                            <label htmlFor="title" className="text-gray-700 font-bold">Telefono</label>
-                                            <input className="w-full rounded-md" type="text" name="title" value={user.phone_number}/>
-                                        </div>
-                                        <div className="form-group">
-                                            <input className="w-40 rounded-md bg-green-400 text-white font-bold font-xl p-2 px-4 border borde-green-600" type="submit" value="GUARDAR" />
-                                        </div>
-                                    </form>
+                                            <div className="form-group grid grid-cols-2 my-2">
+                                                <label htmlFor="title" className="text-left text-gray-700 font-bold">Nombre Completo</label>
+                                                <input className="border border-gray-500 p-1 w-full rounded-md" type="text" name="title" ref={nameUserRef} />
+                                            </div>
+                                            <div className="form-grou grid grid-cols-2 my-2 mb-4">
+                                                <label htmlFor="title" className="text-left text-gray-700 font-bold">Telefono</label>
+                                                <input className="border border-gray-500 p-1 w-full rounded-md" type="text" name="title" ref={phoneUserRef} />
+                                            </div>
+                                            <div className="form-group">
+                                                <input className="border-2 border-gray-300 w-40 rounded-md bg-green-400 text-white font-bold font-xl p-2 px-4 box-content transition delay-150 hover:bg-green-500 hover:border-gray-400 duration-300" type="submit" value="GUARDAR" />
+                                            </div>
+                                        </form>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ) : (
-
-                    <h1>Cargando...</h1>
-
-                )
+                    ) : (
+                        <h1>Cargando...</h1>
+                    )
                 }
-
             </div>
         </div>
     );
--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -1,9 +1,9 @@
-import { BookOpenIcon,FolderAddIcon } from '@heroicons/react/outline';
+import { BookOpenIcon, PencilAltIcon, HandIcon } from '@heroicons/react/outline';
 import { useHttp } from '../hooks/useHttp';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import defaultPicture from '../assets/image/default.png';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export default function Books() {
     const url = 'http://localhost:3900/api/v1';
     //const [books, setBooks] = useHttp(url);
@@ -46,9 +46,9 @@ export default function Books() {
                 </div>
                 <div className="books-list w-full overflow-auto">
                     {(books && books.length > 0) ? (
-                        books.map(book => {
+                        books.map((book,i) => {
                             return (
-                                <div className="book-list-item flex h-sm p-2 my-2 h-60 bg-gray-100">
+                                <div className="book-list-item flex h-sm p-2 my-2 h-60 bg-gray-100" key={'itemBook'+i}>
                                     <div className="book-image w-1/5">
                                         <div className="flex h-60 items-center">
                                             {book.image !== null ? (
@@ -60,17 +60,26 @@ export default function Books() {
                                     </div>
                                     <div className="book-info flex justify-start items-center w-3/5">
                                         <div className="block w-full">
-                                            <h3 className="text-xl font-bold text-gray-700 text-left">{book.title}</h3>
-                                            <div className="text-left text-ellipsis overflow-hidden h-40 px-2">{book.prologue}
+                                            <div className="flex items-center">
+                                                <h3 className="text-2xl font-bold text-gray-700 text-left">{book.title}</h3>
+                                                <h3 className="text-lg font-bold text-gray-500 text-left">-{book.author_name}</h3>
                                             </div>
+                                            <div className="h-40">
+                                                <div className="overflow-hidden h-32 text-ellipsis text-justify">{book.prologue}
+                                                </div>
+                                            </div>
+                                            <div className="font-bold text-green-700 text-left">Disponible</div>
                                         </div>
                                     </div>
                                     <div className="w-1/5">
                                         <h3 className="text-lg text-gray-700 h-1/5">Acciones</h3>
                                         <div className="flex items-center justify-center h-4/5">
                                             <div>
-                                                <Link to={"/loan/create/"+book.id_book} className="flex items-center border-2 border-indigo-400 p-2 rounded-xl w-20 bg-gray-100 hover:bg-indigo-500 hover:text-white transition duration-500">
-                                                    <FolderAddIcon className="h-5 w-5" /> Prestar
+                                                <Link to={"/loan/create/" + book.id_book} className="flex items-center w-24 border-2 border-yellow-400 p-2 rounded-md my-1 bg-gray-100 hover:bg-yellow-500 hover:text-white transition duration-500">
+                                                    <PencilAltIcon className="h-5 w-5" /> Editar
+                                                </Link>
+                                                <Link to={"/loan/create/" + book.id_book} className="flex items-center w-24 border-2 border-blue-400 p-2 rounded-md my-1 bg-gray-100 hover:bg-blue-500 hover:text-white transition duration-500">
+                                                    <HandIcon className="h-5 w-5" /> Prestar
                                                 </Link>
                                             </div>
                                         </div>
